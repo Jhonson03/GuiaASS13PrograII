e03328ad5fd00293ac9698ada4bea997a76df86f
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-10-25 22:23:20 -0600

    AddData

---
+++ Registro/Data/EstudianteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro.Data
+{
+    public class EstudianteContext : DbContext
+    {
+        public DbSet<ClsEstudiante> Estudiante { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=jhonson;Database=Progra2;Trusted_Connection=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=true;");
+        }
+    }
+}
